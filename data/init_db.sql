b2d80bbd69594423d4407ebf034e629da96c3ec0
jadetimea <jade.pouget@eleve.ensai.fr>
2025-10-02 09:34:01 +0000

    enlever les comm

--- data/init_db.sql
+++ data/init_db.sql
@@ -18,16 +18,16 @@ CREATE TABLE utilisateur (
 -----------------------------------------------------
 DROP TABLE IF EXISTS activite CASCADE ;
 
-CREATE TYPE sport AS ENUM ('course', 'natation', 'velo', 'randonnée');  -- Définition de l'énumération
+CREATE TYPE sport AS ENUM ('course', 'natation', 'velo', 'randonnée');  
 
 CREATE TABLE activite (
     id_activite             SERIAL PRIMARY KEY,
-    id_utilisateur          INTEGER,  -- Change SERIAL en INTEGER car ce sera une clé étrangère
-    sport                   sport,    -- Utilisation de l'ENUM sport
+    id_utilisateur          INTEGER,  
+    sport                   sport,   
     date_activite           DATE,
     distance                FLOAT,
     duree                   FLOAT,
-    FOREIGN KEY (id_utilisateur) REFERENCES utilisateur(id_utilisateur)  -- Définition de la clé étrangère
+    FOREIGN KEY (id_utilisateur) REFERENCES utilisateur(id_utilisateur) 
 );
 
 -----------------------------------------------------
@@ -37,12 +37,12 @@ DROP TABLE IF EXISTS commentaire CASCADE ;
 
 CREATE TABLE commentaire (
     id_commentaire          SERIAL PRIMARY KEY,
-    id_activite             INTEGER,  -- Id de l'activité
-    id_auteur               INTEGER,  -- Id de l'auteur
+    id_activite             INTEGER, 
+    id_auteur               INTEGER, 
     commentaire             VARCHAR(300),
     date_commentaire        DATE,
-    FOREIGN KEY (id_activite) REFERENCES activite(id_activite),  -- Clé étrangère vers activite
-    FOREIGN KEY (id_auteur) REFERENCES utilisateur(id_utilisateur)  -- Clé étrangère vers utilisateur
+    FOREIGN KEY (id_activite) REFERENCES activite(id_activite),  
+    FOREIGN KEY (id_auteur) REFERENCES utilisateur(id_utilisateur) 
 );
 
 -----------------------------------------------------
@@ -52,10 +52,10 @@ DROP TABLE IF EXISTS jaime CASCADE ;
 
 CREATE TABLE jaime (
     id_jaime                SERIAL PRIMARY KEY,
-    id_activite             INTEGER,  -- Id de l'activité
-    id_auteur               INTEGER,  -- Id de l'auteur
-    FOREIGN KEY (id_activite) REFERENCES activite(id_activite),  -- Clé étrangère vers activite
-    FOREIGN KEY (id_auteur) REFERENCES utilisateur(id_utilisateur)  -- Clé étrangère vers utilisateur
+    id_activite             INTEGER,  
+    id_auteur               INTEGER, 
+    FOREIGN KEY (id_activite) REFERENCES activite(id_activite),  
+    FOREIGN KEY (id_auteur) REFERENCES utilisateur(id_utilisateur) 
 );
 
 -----------------------------------------------------
@@ -64,9 +64,9 @@ CREATE TABLE jaime (
 DROP TABLE IF EXISTS abonnement CASCADE ;
 
 CREATE TABLE abonnement (
-    id_utilisateur_suiveur INTEGER,  -- Id de l'utilisateur qui suit
-    id_utilisateur_suivi   INTEGER,  -- Id de l'utilisateur suivi
-    PRIMARY KEY (id_utilisateur_suiveur, id_utilisateur_suivi),  -- Clé primaire composée
-    FOREIGN KEY (id_utilisateur_suiveur) REFERENCES utilisateur(id_utilisateur),  -- Clé étrangère vers utilisateur
-    FOREIGN KEY (id_utilisateur_suivi) REFERENCES utilisateur(id_utilisateur)   -- Clé étrangère vers utilisateur
+    id_utilisateur_suiveur INTEGER,  
+    id_utilisateur_suivi   INTEGER,  
+    PRIMARY KEY (id_utilisateur_suiveur, id_utilisateur_suivi),  
+    FOREIGN KEY (id_utilisateur_suiveur) REFERENCES utilisateur(id_utilisateur),  
+    FOREIGN KEY (id_utilisateur_suivi) REFERENCES utilisateur(id_utilisateur)   
 );
